7b92873b1c12a896cce07f7a37884568b6576c51
mikhail <mikhail.kuptsov.03@mail.ru>
2022-12-24 16:12:15 +0300

    Добавьте файлы проекта.

---
+++ Structure_Class_f/Structure_Class_f.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <stdio.h>using namespace std;
+using namespace std;
+/*
+Линейное уравнение y=Ax+B. Поле first - дробное число, коэффициент А;
+поле second – дробное число, коэффициент В.
+Реализовать метод function() – вычисление для заданного x значения функции y.
+*/
+
+struct solution {
+	double first;
+	double second;
+
+	void read() {
+		cout << "A:";
+		cin >> first;
+		cout << "B:";
+		cin >> second;
+	}
+	void display() {
+		cout << "A=" << first << ";" << "B=" << second << endl;
+	}
+
+	void function(int x) {
+		cout << "y=" << first * x + second << endl;
+	}
+};
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	solution one;
+	int a;
+	cout << "Вставьте X:";
+	cin >> a;
+	one.read();
+	one.display();
+	one.function(a);
+	return 0;
+}
